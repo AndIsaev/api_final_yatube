c6ac0a2ba57b4f7a863f6224eb3eeea1e5d59e78
Andrey <and.isaev.v.@yandex.ru>
2020-09-16 15:46:05 +0300

    My project for review

--- api/models.py
+++ api/models.py
@@ -14,20 +14,30 @@ class Group(models.Model):
 
 class Post(models.Model):
     text = models.TextField()
-    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
-    group = models.ForeignKey(Group, on_delete=models.SET_NULL, null=True, related_name='posts')
+    pub_date = models.DateTimeField('Дата публикации',
+                                    auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name='posts')
+    group = models.ForeignKey(Group, on_delete=models.SET_NULL,
+                              null=True, related_name='posts')
 
     def __str__(self):
         return self.text
 
 
 class Comment(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name="comments")
+    post = models.ForeignKey(Post,
+                             on_delete=models.CASCADE,
+                             related_name="comments")
     text = models.TextField()
-    created = models.DateTimeField('Дата добавления', auto_now_add=True, db_index=True)
-    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
+    created = models.DateTimeField('Дата добавления',
+                                   auto_now_add=True,
+                                   db_index=True)
+    pub_date = models.DateTimeField('Дата публикации',
+                                    auto_now_add=True)
 
 
 class Follow(models.Model):
